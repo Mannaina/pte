5e0fa6f90fffae6d8a98889609856efa3be36099
Sunaina Passi <sunaina.passi4@gmail.com>
2018-05-20 17:25:32 +0530

    State edit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Input from "./Input"
 import logo from './logo.svg';
 import './App.css';
+import './Position.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='background'>
         <Input/>
       </div>
     );
--- src/Input.js
+++ src/Input.js
@@ -1,29 +1,57 @@
 import React from 'react';
 import './Position.css'
 class Input extends React.Component {
+    constructor(){
+        super()
+           this.state = {
+               text:null,
+               password:null,
+               date:null
+           }
+        
+    }
+
+    onChange=(e)=>{
+        this.setState({
+            text:e.target.value
+        })
+    }
+    onChangePassword=(e)=>{
+        this.setState({
+            password:e.target.value
+        })
+    }
+    onChangeDate=(e)=>{
+        this.setState({
+date:e.target.value
+        })
+    }
     render() {
         return (
-            <div>
+            <div className="background">
                 <div className="position1"> 
-                    <h3>Welcome To Online PTE Forms</h3>
+                    <h3>!Welcome To Online PTE Forms!</h3>
                 </div>
                 <div className='fixed'>
                     <div>
-                        Name <input type="text" />
+                        Name <input type="text" onChange={(e)=>{this.onChange(e)}}/>
+                    {console.log(this.state.text)}
                     </div>
                     <br />
                     <div>
-
-                        Passport No.  <input type="text" />
+                        Passport No.  <input type="text" onChange={(e)=>{this.onChangePassword(e)}}/>
+                        {console.log(this.state.password)}
                     </div>
                     <br />
                     <div>
-                        Test Date <input type="date" />
+                        Test Date <input type="date" onChange={(e)=>{this.onChangeDate(e)}}/>
+                        {console.log(this.state.date)}
                     </div>
                 </div>
+                
             </div>
+        
         );
-
     }
 }
 
